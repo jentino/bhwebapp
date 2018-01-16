ba621f229c9882bff7b405b465e1297fcdc5ac7d
jenty <jentino@gmail.com>
2018-01-16 03:48:44 +0200

    s

--- server.js
+++ server.js
@@ -1,6 +1,28 @@
-var http = require('http');
+'use strict';
+//version 1.1.1
+const express = require('express');
+const SocketServer = require('ws').Server;
+const path = require('path');
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World!');
-}).listen(80);
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, 'index.html');
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX) )
+  .use(express.static('js'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+const wss = new SocketServer({ server });
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+  ws.on('close', () => console.log('Client has disconnected'));
+});
+
+setInterval(() => {
+  wss.clients.forEach((client) => {
+    client.send(new Date().toTimeString());
+  });
+}, 1000);
