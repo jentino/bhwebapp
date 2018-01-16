9223979400c96b72a6ac0f92e00ab735854605f0
jenty <jentino@gmail.com>
2018-01-16 04:57:01 +0200

    sd

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
 
 const server = express()
+  .set('view engine', 'ejs')
   .use((req, res) => res.sendFile(INDEX) )
   .use(express.static('js'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
