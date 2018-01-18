f3d235ab26263933567531fbabb10364a4c7a7b1
jenty <jentino@gmail.com>
2018-01-18 02:49:26 +0200

    d

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const INDEX = path.join(__dirname, 'index.html');
 const server = express()
   .set('view engine', 'ejs')
   .use((req, res) => res.sendFile(INDEX))
-  .use(express.static('js'))
+  .use(express.static('knob'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 ///////////////////////////////////////////////////////////////////////////////////////////
