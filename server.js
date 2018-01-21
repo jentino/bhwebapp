20ceba4368e0794043ad2232d7823fed68467f49
jenty <jentino@gmail.com>
2018-01-21 04:28:28 +0200

    feafrt

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict';
 //version 1.1.1
 var express = require('express');
+const SocketServer = require('ws').Server;
 const path = require('path');
 
 const PORT = process.env.PORT || 3000;
@@ -12,41 +13,29 @@ var app = express()
 .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 
-// const express = require('express');
-// const SocketServer = require('ws').Server;
-// const path = require('path');
-
-// const PORT = process.env.PORT || 3000;
-// const INDEX = path.join(__dirname, 'index.html');
-// // const PUB = path.join(__dirname, 'public/');
-
-
-// const server = express()
 //   // .set('view engine', 'ejs')
-//   // .use(express.static(__dirname + '/public'))
-//   .use((req, res) => res.sendFile(INDEX))
-//   .use(express.static(PUB))
+
 
 // ///////////////////////////////////////////////////////////////////////////////////////////
 
-// const wss = new SocketServer({ server });
+const wss = new SocketServer({ app });
 
-// wss.on('connection', (ws) => {
-//   console.log('Client connected');
-//   ws.on('close', () => console.log('Client has disconnected'));
-// });
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+  ws.on('close', () => console.log('Client has disconnected'));
+});
 
-// setInterval(() => {
-//   wss.clients.forEach((client) => {
-//     // var d = new Date(); // The 0 there is the key, which sets the date to the epoch
-//   	// // d.setUTCSeconds(thetime);
+setInterval(() => {
+  wss.clients.forEach((client) => {
+    // var d = new Date(); // The 0 there is the key, which sets the date to the epoch
+  	// // d.setUTCSeconds(thetime);
 	
-//     // var globalSeconds = ;
+    // var globalSeconds = ;
 
-//     // var dt = 3;
-//     // var dt2 = dt.getSeconds();
+    // var dt = 3;
+    // var dt2 = dt.getSeconds();
   
-//     // var timenow = new Date().toTimeString();
-//     client.send(new Date('July 20, 69 00:20:18'));
-//   });
-// }, 1000);
+    // var timenow = new Date().toTimeString();
+    client.send(new Date());
+  });
+}, 1000);
