1bd7d0bb5063e0005ecca5b2df951cc88af6251f
jenty <jentino@gmail.com>
2018-01-22 20:12:44 +0200

    asdf

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
-const ABOUT = path.join(__dirname, 'about.html');
+// const ABOUT = path.join(__dirname, 'about.html');
 
 var app = express()
 .use('/resource', express.static( "public" ) )
@@ -26,17 +26,17 @@ wss.on('connection', (ws) => {
   ws.on('close', () => console.log('Client has disconnected'));
 });
 
-setInterval(() => {
-  wss.clients.forEach((client) => {
-    // var d = new Date(); // The 0 there is the key, which sets the date to the epoch
-  	// // d.setUTCSeconds(thetime);
+// setInterval(() => {
+//   wss.clients.forEach((client) => {
+//     // var d = new Date(); // The 0 there is the key, which sets the date to the epoch
+//   	// // d.setUTCSeconds(thetime);
 	
-    // var globalSeconds = ;
-    // new Date().toTimeString()
-    // var dt = 3;
-    // var dt2 = new Date().getSeconds().toTimeString();
+//     // var globalSeconds = ;
+//     // new Date().toTimeString()
+//     // var dt = 3;
+//     // var dt2 = new Date().getSeconds().toTimeString();
   
-    // var timenow = new Date().toTimeString();
-    client.send("test");
-  });
-}, 1000);
+//     // var timenow = new Date().toTimeString();
+//     client.send("test");
+//   });
+// }, 1000);
