06510ac438dcc9cb956b1753007f12e1769a4696
jenty <jentino@gmail.com>
2018-02-08 04:34:57 +0200

    we

--- assets/resources/spectroReferral.js
+++ assets/resources/spectroReferral.js
@@ -1,5 +1,29 @@
 var ids=["referralId","refId","ref"]
-for(i=0;i<ids.length;i++){var referralId=getUrlParameterValue(ids[i]);if(referralId!=null){var url="https://spectrocoin.com/en/signup.html?referralId="+ referralId
-var xmlHttp;if(window.XMLHttpRequest){xmlHttp=new XMLHttpRequest();}else{xmlHttp=new ActiveXObject("Microsoft.XMLHTTP");}
-xmlHttp.open("GET",url,true);xmlHttp.withCredentials=true;xmlHttp.send(null);break;}}
-function getUrlParameterValue(name){var url=location.href;name=name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");var regexS="[\\?&]"+ name+"=([^&#]*)";var regeX=new RegExp(regexS);var results=regeX.exec(url);return results==null?null:results[1];}
+
+for(i=0;i<ids.length;i++){
+
+	var referralId = getUrlParameterValue(ids[i]);
+
+	if(referralId!=null){
+			var url="https://spectrocoin.com/en/signup.html?referralId="+ referralId
+			var xmlHttp;
+			if(window.XMLHttpRequest){
+				xmlHttp = new XMLHttpRequest();
+			}else{
+				xmlHttp=new ActiveXObject("Microsoft.XMLHTTP");
+			}
+
+			xmlHttp.open("GET",url,true);
+			xmlHttp.withCredentials=true;
+			xmlHttp.send(null);
+			break;
+	}
+}
+function getUrlParameterValue(name){
+	var url=location.href;
+	name=name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
+	var regexS="[\\?&]"+ name+"=([^&#]*)";
+	var regeX=new RegExp(regexS);
+	var results=regeX.exec(url);
+	return results==null?null:results[1];
+}
