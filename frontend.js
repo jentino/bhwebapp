6574aacfb495ec7c2a6ce0f96b47f7cf690c5a22
jenty <jentino@gmail.com>
2018-03-05 19:13:36 +0200

    sldasf

--- frontend.js
+++ frontend.js
@@ -3,11 +3,19 @@
 $(function () {
     "use strict";
 
-    var timer = $('#timer');
-    var status = $('#status');
+    // var timer = $('#timer');
     // var mybalance = $('#mybalance');
 
-    var connection = new WebSocket('ws://127.0.0.1:1337');
+    var HOST = location.origin.replace(/^http/, 'ws')
+    var ws = new WebSocket(HOST);
+
+
+    var timer = $('#servertime');
+    var status = $('#status');
+
+
+
+    // var connection = new WebSocket('ws://127.0.0.1:1337');
 
     connection.onopen = function () {   
         status.text('Connected.');
@@ -43,35 +51,35 @@ $(function () {
         }
     }, 3000);
 
-    $("#btndisconnect").attr("disabled", true);
-    // $.fn.startClock = function() {
-    //     connection.send('startclock');
-    //     $("#btnstart").attr("disabled", true);
-    //     $("#btnstop").attr("disabled", false);
+    // $("#btndisconnect").attr("disabled", true);
+    // // $.fn.startClock = function() {
+    // //     connection.send('startclock');
+    // //     $("#btnstart").attr("disabled", true);
+    // //     $("#btnstop").attr("disabled", false);
+    // //     return this;
+    // //  };
+
+    // //  $.fn.stopClock = function() {
+    // //     connection.send('stopclock');
+    // //     $("#btnstart").attr("disabled", false);
+    // //     $("#btnstop").attr("disabled", true);
+    // //     return this;
+    // //  };
+
+    //  $.fn.connectmaster = function() {
+    //     connection.send(JSON.stringify({ data: 'startServerClock' }));
+    //     $("#btnconnectmaster").attr("disabled", true);
+    //     $("#btndisconnectmaster").attr("disabled", false);
     //     return this;
     //  };
 
-    //  $.fn.stopClock = function() {
-    //     connection.send('stopclock');
-    //     $("#btnstart").attr("disabled", false);
-    //     $("#btnstop").attr("disabled", true);
+    //  $.fn.disconnectmaster = function() {
+    //     //connection.send('stopServerClock');
+    //     connection.send(JSON.stringify({ data: 'stopServerClock' }));
+    //     $("#btnconnectmaster").attr("disabled", false);
+    //     $("#btndisconnectmaster").attr("disabled", true);
     //     return this;
     //  };
-
-     $.fn.connectmaster = function() {
-        connection.send(JSON.stringify({ data: 'startServerClock' }));
-        $("#btnconnectmaster").attr("disabled", true);
-        $("#btndisconnectmaster").attr("disabled", false);
-        return this;
-     };
-
-     $.fn.disconnectmaster = function() {
-        //connection.send('stopServerClock');
-        connection.send(JSON.stringify({ data: 'stopServerClock' }));
-        $("#btnconnectmaster").attr("disabled", false);
-        $("#btndisconnectmaster").attr("disabled", true);
-        return this;
-     };
 
 
 
